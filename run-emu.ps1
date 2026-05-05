6d40089a945a105c777c2c71d258b043449810c9
Nicolas Berenguela <103396195+RazorLazor27@users.noreply.github.com>
2026-05-04 20:14:32 -0400

    se añade comando para correr emulador en WINDOWS

---
+++ run-emu.ps1
@@ -0,0 +1,9 @@
+# Script para lanzar el emulador sin abrir Android Studio
+$emulatorPath = "m:\Android_SDK\emulator\emulator.exe"
+$avdName = "Medium_Phone"
+
+Write-Host "Iniciando emulador: $avdName..." -ForegroundColor Cyan
+
+# El '&' es para ejecutar el comando y '-detachable' permite que el emulador 
+# siga vivo aunque cierres la terminal (dependiendo de la versión del SDK)
+Start-Process $emulatorPath -ArgumentList "-avd", $avdName, "-no-snapshot-load"
